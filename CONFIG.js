126ee7ea584f37b2055045d65745cfe962faf23d
guiyuan <guiyuan@newborntown.com>
2022-02-22 11:10:57 +0800

    upd

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Làm người yêu tớ nhé I Love You",
+  titleHeader: "Be my love I Love You",
   descriptionHeader:
-    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
-  buttonYes: "Đồng Ý",
-  buttonNo: "Không! Còn lâu nhé.",
-  titleModar: "Cảm ơn em vì đồng ý",
+    "如果你退出或者不回复，说明你同意了 ^^",
+  buttonYes: "我愿意",
+  buttonNo: "考虑考虑",
+  titleModar: "谢谢你同意了",
   descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    ' 我就知道你会同意做我的爱人^^ </br>谢谢你同意让我保护和爱护你。对我来说，你永远是个小女孩:)) </br>只要你不放手，我会一直陪着你，you have my promise。</br> 执子之手，与子偕老。',
 };

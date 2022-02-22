d9ff4a3dbfe3e6722b014bc3d6c0ed05a9ece2ec
guiyuan <guiyuan@newborntown.com>
2022-02-22 11:18:40 +0800

    upd

--- CONFIG.js
+++ CONFIG.js
@@ -6,5 +6,5 @@ const CONFIGDATA = {
   buttonNo: "考虑考虑",
   titleModar: "谢谢你同意了",
   descriptionModar:
-    ' 我就知道你会同意做我的爱人^^ </br>谢谢你同意让我保护和爱护你。对我来说，你永远是个小女孩:)) </br>只要你不放手，我会一直陪着你，you have my promise。</br> 执子之手，与子偕老。',
+    "我就知道你会同意做我的爱人^^ </br>谢谢你同意让我保护和爱护你。对我来说，你永远是个小女孩:)) </br>只要你不放手，我会一直陪着你，you have my promise。</br> 执子之手，与子偕老。",
 };
